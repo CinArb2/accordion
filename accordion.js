40059e2827edfed292fd3adbb16e1dfd26c08222
CinArb2 <91969387+CinArb2@users.noreply.github.com>
2021-11-11 07:07:25 -0500

    height auto

--- accordion.js
+++ accordion.js
@@ -1,14 +1,31 @@
 let cardQA = document.querySelectorAll('.card__qa-title');
+let cardText = document.querySelectorAll('.card__qa-text');
+
+
 
 cardQA.forEach(item => {
+
+  // item.nextElementSibling.style.transition = "all 300ms";
+
   item.addEventListener('click', () => {
+    
     cardQA.forEach(item2 => {
       if (item != item2) {
+        item2.nextElementSibling.style.height = null;
         item2.nextElementSibling.classList.remove('show')
         item2.classList.remove('show');
       }
     })
+    
+
+    item.nextElementSibling.style.height = 'auto';
+    let x = item.nextElementSibling.clientHeight;
+    item.nextElementSibling.style.height = x;
     item.nextElementSibling.classList.toggle('show');
     item.classList.toggle('show');
+    if (!item.nextElementSibling.classList.contains('show')) {
+      item.nextElementSibling.style.height = null;
+    }
+    
   })
 })
